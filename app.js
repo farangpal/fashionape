985eb37543bb6afab8efaa1972382b9623251124
farangpal <98533096+farangpal@users.noreply.github.com>
2022-02-16 11:52:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const statusp = document.querySelector("#status");
 const connectBtn = document.querySelector('#connectBtn');
 const checkoutBtn = document.querySelector('#checkoutBtn');
 //const connectBtnHeader = document.querySelector('#connectBtnHeader');
-const pricePerNFT = 0.2;
+const pricePerNFT = 0.16;
 
 /** input number spinner
  */
